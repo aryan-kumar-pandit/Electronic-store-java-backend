97ebee27ff34d1bd049b4c1075e3718c3cb71605
aryan kumar <aryankumarpandito@gmail.com>
2024-07-21 05:06:39 +0530

    swagger implemented springfox has been depreceted

--- src/main/java/com/lcwd/electronic/store/ElectronicStoreApplication.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStoreApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+
 import java.util.List;
 import java.util.UUID;
 
