1eb59d64a21134a1dc20fad8aac0d85326cb8c5a
aryan kumar <aryankumarpandito@gmail.com>
2024-07-20 10:31:52 +0530

    configure Jwt in security.config file

--- src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
+++ src/main/java/com/lcwd/electronic/store/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.lcwd.electronic.store.config;
 
+import com.lcwd.electronic.store.security.JwtAuthenticationEntryPoint;
+import com.lcwd.electronic.store.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,14 +10,22 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 //@EnableWebSecurity(debug = true)
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    JwtAuthenticationFilter filter;
+    @Autowired
+    JwtAuthenticationEntryPoint entryPoint;
+
     //security filter chain bean
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception
@@ -36,7 +47,13 @@ public class SecurityConfig {
 
         });
         //kind of security you want-- we used basic security
-        security.httpBasic(Customizer.withDefaults());
+        //security.httpBasic(Customizer.withDefaults());
+        //entry point
+        security.exceptionHandling(ex-> ex.authenticationEntryPoint(entryPoint));
+        //session management policy
+        security.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //added jwt filter before any other filter
+        security.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
         return security.build();
     }
